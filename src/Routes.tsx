a1700055765e9643d1aabd2e735c6cb02f937b8b
eydrianJS <adrian.olszowski@wp.pl>
2020-04-04 14:31:25 +0200

    initial

---
+++ src/Routes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Switch, Route }  from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
